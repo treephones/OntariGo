6e77704656451d32ee53d92c7b45db5ca3fba2d4
Moez B <moezbajwa@hotmail.com>
2021-02-03 17:57:41 -0500

    added navbar component

---
+++ ontarigo-frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return (<p>navbar here lol</p>);
+    }
+}
+ 
+export default Navbar;
